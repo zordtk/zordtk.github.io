083f22f77b0c4f01bcbeead876cff09e86f5b19f
Jeremy Harmon <jeremy.harmon@zoho.com>
2016-04-07 18:40:00 -0400

    Updated docs

--- ustring_docs/search/all_4.js
+++ ustring_docs/search/all_4.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['end',['end',['../class_u_string.html#a5abc45285a66adc36ec520be526f72d9',1,'UString::end()'],['../class_u_string.html#a81a92c80331047fdd52205757ae64130',1,'UString::end() const ']]]
+  ['end',['end',['../class_u_string.html#a5abc45285a66adc36ec520be526f72d9',1,'UString::end()'],['../class_u_string.html#a81a92c80331047fdd52205757ae64130',1,'UString::end() const ']]],
+  ['erase',['erase',['../class_u_string.html#a90dd1f6a0a5ce66c9604957db98655ec',1,'UString::erase(const Iterator &amp;start, const Iterator &amp;stop)'],['../class_u_string.html#aec3cdb5d9af84ebc8a620e81b501c338',1,'UString::erase(std::size_t start, std::size_t num=1)']]]
 ];
