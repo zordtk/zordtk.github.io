4db1d3b854a4e24b338a1780695e28d5f5b00753
Jeremy Harmon <jeremy.harmon@zoho.com>
2016-04-07 22:35:17 -0400

    Updated docs

--- ustring_docs/search/all_7.js
+++ ustring_docs/search/all_7.js
@@ -1,5 +1,6 @@
 var searchData=
 [
-  ['iteratorbase',['IteratorBase',['../class_u_string_1_1_iterator_base.html',1,'UString']]],
-  ['iteratorbase',['IteratorBase',['../class_u_string_1_1_iterator_base.html#aa86286dc3c9f7ad255f3c72889da6e11',1,'UString::IteratorBase']]]
+  ['insert',['insert',['../class_u_string.html#a947f406da35c173765d41b3940c76080',1,'UString']]],
+  ['iteratorbase',['IteratorBase',['../class_u_string_1_1_iterator_base.html#aa86286dc3c9f7ad255f3c72889da6e11',1,'UString::IteratorBase']]],
+  ['iteratorbase',['IteratorBase',['../class_u_string_1_1_iterator_base.html',1,'UString']]]
 ];
--- ustring_docs/search/functions_6.js
+++ ustring_docs/search/functions_6.js
@@ -1,4 +1,5 @@
 var searchData=
 [
+  ['insert',['insert',['../class_u_string.html#a947f406da35c173765d41b3940c76080',1,'UString']]],
   ['iteratorbase',['IteratorBase',['../class_u_string_1_1_iterator_base.html#aa86286dc3c9f7ad255f3c72889da6e11',1,'UString::IteratorBase']]]
 ];
